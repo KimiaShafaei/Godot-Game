e1bc8e9b0c96a0f83c6786a32540f249e58c3365
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-02-27 14:24:29 -0800

    fix audio

--- Scripts/intro_ui.gd
+++ Scripts/intro_ui.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-
+@onready var start_audio = $start_music
 
 func _on_start_pressed() -> void:
+	start_audio.stop()
 	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")
-
-
 
 
 
