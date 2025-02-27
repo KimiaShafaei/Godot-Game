de52862c7462f4fb026b3005ca55212a473be26e
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-02-27 04:40:58 -0800

    add start UI

---
+++ Scripts/intro_ui.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")
+
+
+
+
+
+func _on_menu_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
