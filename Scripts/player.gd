8bf70c616d40468da4e3a173626e150a60fc10e0
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-02-27 10:01:07 -0800

    add open box and walking audios

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 @export var speed = 200
 @onready var anim = $AnimatedSprite2D
 @onready var tile_map = $"../TileMapLayer"
+@onready var walking_sound = $walking
+@onready var success_sound = $"../success"
+@onready var open_box_sound = $"../open_box"
 
 var _path : Array = []
 var _current_index = 0
@@ -49,8 +52,10 @@ func _physics_process(delta):
 			
 		if global_position.distance_to(end_point) < 10:
 			print("Reached to endpoind. The game is over!")
+			if open_box_sound and not open_box_sound.playing:
+				open_box_sound.play()
 			open_box()
-			await get_tree().create_timer(2.0).timeout
+			await get_tree().create_timer(1.0).timeout
 			get_tree().quit()
 		move_and_slide()
 	else:
@@ -74,7 +79,10 @@ func _update_animation(direction):
 		else :
 			anim.play("Walk_up")
 			_last_side = "up"
-
+	
+	if direction.length() > 0 and not walking_sound.playing:
+		walking_sound.play()
+		
 func _play_idle():
 	if _last_side == "right":
 		anim.flip_h = false
@@ -86,6 +94,9 @@ func _play_idle():
 		anim.play("Idle_up")
 	else :
 		anim.play("Idle_down")
+		
+	if walking_sound.playing:
+		walking_sound.stop()
 
 func open_box():
 	var box_node = get_node("../box")
