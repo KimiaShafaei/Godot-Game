88d193c3d3f23af5b4a7261e0fa271fe6221fbd8
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-03-07 10:10:44 -0800

    add level 2 scene and enemy

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,20 +1,33 @@
 extends CharacterBody2D
 
-const speed = 20
+@export var radius: float = 50
+@export var speed: float = 1.0
+@onready var enemy_anim = $AnimatedSprite2D
 
-@export var player: Node2D
-@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+var angle: float = 0
+var center: Vector2 
 
+func _ready():
+	center = global_position
+
+func _process(delta):
+	angle += speed * delta 
+	var new_position = center + Vector2(radius * cos(angle), radius * sin(angle))
 	
-func _physics_process(delta: float) -> void:
-	var direction = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = direction * speed
-	move_and_slide()
+	var direction = (new_position - global_position).normalized()
+	update_animation(direction)
 	
-func makePath() -> void:
-	nav_agent.target_position = player.global_position
+	global_position = new_position
 
 
-
-func _on_timer_timeout() -> void:
-	makePath()
+func update_animation(direction: Vector2):
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			enemy_anim.play("Walk_right")
+		else:
+			enemy_anim.play("Walk_left")
+	else :
+		if direction.y > 0:
+			enemy_anim.play("Walk_down")
+		else :
+			enemy_anim.play("Walk_up")
