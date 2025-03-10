4dbd5d3781a2ac87e7f1ff1d69d0851f736752f1
KimiaShafaei <169140834+KimiaShafaei@users.noreply.github.com>
2025-03-10 14:01:09 -0700

    use path2d for walking and chasing the enemy

---
+++ Scripts/enemy_path_follow_2d.gd
@@ -0,0 +1,43 @@
+extends PathFollow2D
+
+@export var speed: float = 50
+@export var chase_speed:float = 80
+@export var detect_range: float = 100
+
+@onready var enemy_anim = $Enemy/AnimatedSprite2D
+
+var player = null
+var chasing = false
+var last_position = Vector2.ZERO
+
+func _ready() :
+	player = get_tree().get_nodes_in_group("player")[0]
+	last_position = global_position
+	
+func _process(delta: float) -> void:
+	if player and player.position.distance_to(global_position) < detect_range:
+		chasing = true
+	else :
+		chasing = false
+	
+	if player and chasing:
+		var direction = (player.global_position - global_position).normalized()
+		global_position += direction * chase_speed * delta
+		_update_animation(direction)
+	else :
+		progress += speed * delta
+		_update_animation(global_position - last_position)
+	last_position = global_position
+		
+func _update_animation(direction:  Vector2):
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			enemy_anim.play("Walk_right")
+		else:
+			enemy_anim.play("Walk_left")
+	else :
+		if direction.y > 0:
+			enemy_anim.play("Walk_down")
+		else :
+			enemy_anim.play("Walk_up")
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -62,6 +62,7 @@ func _physics_process(_delta):
 			open_box()
 			success_sound.play()
 			await get_tree().create_timer(1.0).timeout
+			get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
 		move_and_slide()
 	else:
 		velocity = Vector2.ZERO
