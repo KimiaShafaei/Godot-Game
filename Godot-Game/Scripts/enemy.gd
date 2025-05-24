612849a603253d124c7ed2894bb43deee8460115
behnud1382 <149939590+behnud1382@users.noreply.github.com>
2025-05-24 10:14:41 -0700

    Add the waypoint node in leve2

--- Godot-Game/Scripts/enemy.gd
+++ Godot-Game/Scripts/enemy.gd
@@ -3,21 +3,21 @@ extends Node
 var state_manager
 
 func _ready():
-    state_manager = preload("res://Scripts/state_manager.gd").new()
-    state_manager.initialize(self)
-    state_manager.change_state("idle")
+	state_manager = preload("res://Scripts/state_manager.gd").new()
+	state_manager.initialize(self)
+	state_manager.change_state("idle")
 
 func _process(delta):
-    state_manager.update(delta)
+	state_manager.update(delta)
 
 func on_player_detected():
-    state_manager.change_state("run")
+	state_manager.change_state("run")
 
 func on_player_out_of_range():
-    state_manager.change_state("walk")
+	state_manager.change_state("walk")
 
 func on_health_low():
-    state_manager.change_state("die")
+	state_manager.change_state("die")
 
 func on_idle_timeout():
-    state_manager.change_state("idle")
+	state_manager.change_state("idle")
